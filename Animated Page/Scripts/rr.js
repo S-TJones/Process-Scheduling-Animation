f5a318e197dddb1b4faf63136771b6e9f6c044e1
Shemar Jones <jones.shemar.sj@gmail.com>
2021-11-27 22:49:50 -0500

    Round Robin, running

--- Animated Page/Scripts/rr.js
+++ Animated Page/Scripts/rr.js
@@ -74,61 +74,48 @@ function swap(xp, yp)
 
 start = async function(){
     // setTimeout(function, milliseconds)
-    var waitTime = 5;
+    var waitTime = document.getElementById("timer").value;
+
+    if (waitTime !== "") {
+        waitTime = parseInt(waitTime);
+    } else {
+        waitTime = 2;
+    }
     
-
+    //
     while (lst.length > 0){
-        var popList = [];
+        var process_obj = lst[0];
 
-        for(var x = 0; x < lst.length; x++){
-
-            var time = lst[x].time;
-            var remainingTime = time - waitTime;
-            console.log(lst[x].id + " Rem: " + remainingTime);
+        var time = process_obj.time;
+        var remainingTime = time - waitTime;
+        console.log(process_obj.id + "| " + process_obj.time + "-" + waitTime + " Rem: " + remainingTime);
 
 
-            if (remainingTime === 0) {
-                // Executes for designated wait time
-                for(var x = 0; x < waitTime; x++){
-                    await new Promise(done => setTimeout(() => done(), 1000));
-                }
-                popList.push(x);
-                console.log("Match Zero");
+        if (remainingTime <= 0) {
+            // Executes for designated wait time
+            for(var x = 0; x < time; x++){
+                await new Promise(done => setTimeout(() => done(), 1000));
             }
-            else if (remainingTime < 0) {
-                // Executes for designated wait time
-                for(var x = 0; x < time; x++){
-                    await new Promise(done => setTimeout(() => done(), 1000));
-                }
-                popList.push(x);
-                console.log("Less than Zero");
-            }
-            else {
-                // Executes for designated wait time
-                for(var x = 0; x < waitTime; x++){
-                    await new Promise(done => setTimeout(() => done(), 1000));
-                }
-
-                
-                // Update time
-                lst[x].time = remainingTime;
-                console.log("Updating: " + lst[x].id + " with " + remainingTime);
-            }
-
+            
+            // console.log("Less than or equal to Zero");
+            console.log("Popping " + process_obj.id);
+            popProcess(0, false);
         }
-        
-        for (x in popList) {
-            console.log("Popping " + x);
-            popProcess(x, false);
+        else {
+            // Executes for designated wait time
+            for(var y = 0; y < waitTime; y++){
+                await new Promise(done => setTimeout(() => done(), 1000));
+            }
+
+            // Update time
+            console.log("Updating: " + process_obj.id + " with " + remainingTime);
+            var updated_obj = {id:(process_obj.id), time:remainingTime};
+            lst.push(updated_obj);
+            
+            console.log("Popping " + process_obj.id);
+            popProcess(0, false);
         }
-        console.log("Length: " + lst.length);
-        // 7, 2, 5, 3, 4
-        // 5, 0, 3, 1, 2
-        // 5, 3, 1, 2
-        // 3, 1, -1, 0
-        // 3, 1
-        // 1, -1
-        // -1
+
     }
 
     console.log("DONE");
