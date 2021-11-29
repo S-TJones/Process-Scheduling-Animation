85af12052d53ae13d55d638e8ab2092f9b3e62e3
Shemar Jones <jones.shemar.sj@gmail.com>
2021-11-28 20:12:18 -0500

    Round Robin updated CSS

--- Animated Page/Scripts/rr.js
+++ Animated Page/Scripts/rr.js
@@ -1,6 +1,7 @@
 
 // Global List for Processes
 var lst = [];
+var fin = [];
 
 addProcesses = function(event) {
     event.preventDefault();
@@ -45,23 +46,36 @@ popProcess = function(index=-1, execute=false) {
     lst.splice(index, 1);
     
     // Get the current process objects
-    var processes = '';
-    for (var i = 0; i < lst.length; i++) {
-        let obj = lst[i];
-        let name = obj.id;
-        let number = obj.time;
-        processes += '<div class="process">' + name + "-" + number + 'ms</div>';
-    }
+    var processes = updateProcessSection();
 
     // Add the processes
     document.getElementById("processes").innerHTML = processes;
     
     // Check if the Process should be executed
     if (execute) {
-        document.getElementById("executed").innerHTML = '<div class="process">' + removed.id + "-" + removed.time + 'ms</div>';
+        document.getElementById("executed").innerHTML = '<div class="execution-box box-up-row"><div class="process green">' + removed.id + "-" + removed.time + 'ms</div></div>';
     }
 
     return removed;
+}
+
+function updateProcessSection(the_list=lst, middle=true) {
+    // Get the current process objects
+    var processes = '';
+    for (var i = 0; i < the_list.length; i++) {
+        let obj = the_list[i];
+        let name = obj.id;
+        let number = obj.time;
+
+        if (middle) {
+            processes += '<div class="process box-up-col">' + name + "-" + number + 'ms</div>';
+        } else {
+            processes += '<div class="process red">' + name + '</div>';
+        }
+        
+    }
+
+    return processes;
 }
 
 // Javascript program for implementation of selection sort
@@ -71,59 +85,98 @@ function swap(xp, yp)
     lst[xp] = lst[yp];
     lst[yp] = temp;
 }
+ 
+function selectionSort(n){
+    var i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++){
+
+            var x, y;
+            x = lst[j].time;
+            y = lst[min_idx].time;
+
+            // console.log("Compare: " + x + " vs "+ y);
+            if (x < y){
+                min_idx = j;
+                // console.log("Smallest= " + min_idx);
+            }
+        }
+        // Swap the found minimum element with the first element
+        swap(min_idx, i);
+    }
+}
 
 start = async function(){
-    // setTimeout(function, milliseconds)
-    var waitTime = document.getElementById("timer").value;
-
-    if (waitTime !== "") {
-        waitTime = parseInt(waitTime);
-    } else {
-        waitTime = 2;
-    }
     
     //
     while (lst.length > 0){
-        var process_obj = lst[0];
 
-        var time = process_obj.time;
-        var remainingTime = time - waitTime;
+        var waitTime = document.getElementById("timer").value;
+
+        if (waitTime !== "") {
+            waitTime = parseInt(waitTime);
+        } else {
+            waitTime = 2;
+        }
+
+        var process_obj = lst[0];
+        var process_time = process_obj.time;
+        var remainingTime = process_time - waitTime;
         console.log(process_obj.id + "| " + process_obj.time + "-" + waitTime + " Rem: " + remainingTime);
 
+        popProcess(0, true); // Pop, then...
 
+        // 
         if (remainingTime <= 0) {
-            // Executes for designated wait time
-            for(var x = 0; x < time; x++){
-                await new Promise(done => setTimeout(() => done(), 1000));
-            }
-            
-            // console.log("Less than or equal to Zero");
-            console.log("Popping " + process_obj.id);
-            popProcess(0, true);
-        }
-        else {
-            // Executes for designated wait time
-            for(var y = 0; y < waitTime; y++){
-                await new Promise(done => setTimeout(() => done(), 1000));
+            for (let index = 1; index < process_time+1; index++) {
+                // Do a complete second
+                var speed = document.getElementById("slow_fast").checked;
+                if(speed){
+                    await new Promise(done => setTimeout(() => done(), 1000));
+                } else {
+                    await new Promise(done => setTimeout(() => done(), 3000));
+                }
+                console.log("WAIT "+process_time + " "+ index+"="+(process_time-index));
+
+                document.getElementById("executed").innerHTML = '<div class="execution-box box-up-row"><div class="process green">' + process_obj.id + "-" + (process_time-index) + 'ms</div></div>';
             }
 
-            // Update time
+            fin.push(process_obj);
+            var result = updateProcessSection(fin, false);
+            document.getElementById("finished").innerHTML = result;
+
+        } else {
+            for (let index = 1; index < waitTime+1; index++) {
+                // Do a complete second
+                var speed = document.getElementById("slow_fast").checked;
+                if(speed){
+                    await new Promise(done => setTimeout(() => done(), 1000));
+                } else {
+                    await new Promise(done => setTimeout(() => done(), 3000));
+                }
+                console.log("WAIT "+process_time + " "+ index+"="+(process_time-index));
+
+                document.getElementById("executed").innerHTML = '<div class="execution-box box-up-row"><div class="process green">' + process_obj.id + "-" + (process_time-index) + 'ms</div></div>';
+            }
+
             console.log("Updating: " + process_obj.id + " with " + remainingTime);
             var updated_obj = {id:(process_obj.id), time:remainingTime};
             lst.push(updated_obj);
-            
-            console.log("Popping " + process_obj.id);
-            popProcess(0, true);
         }
-
     }
 
+    document.getElementById("executed").innerHTML = "";
     console.log("DONE");
 }
 
 // Window Onload
 window.onload = function() {
-    console.log('window - onload');
+    console.log('window - onload {Round Robin}');
 
     // Buttons
     var inputButton = document.getElementById("input");
@@ -134,7 +187,10 @@ window.onload = function() {
     // Reset Queue
     resetButton.addEventListener("click", function(){
         lst = [];
+        fin = [];
         document.getElementById("processes").innerHTML = "";
+        document.getElementById("executed").innerHTML = "";
+        document.getElementById("finished").innerHTML = "";
     });
     // Check for Input
     inputButton.addEventListener("click", addProcesses);
